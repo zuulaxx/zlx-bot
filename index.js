0f6bb9ecd127878d23c449c840fa8b5b6b96e6a5
zuulaxx <guigui.8929@gmail.com>
2022-07-05 17:13:25 +0200

    a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//aaaaaaaaaaaaaaaa
+//aaaaaaaaaaaaaa
 const Discord = require('discord.js');
 const { MessageEmbed, Collection, SlashCommandBuilder, Formatters } = require('discord.js');
 require('@discordjs/voice');
